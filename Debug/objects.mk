4cc735812dc4d6cca072efb34a840718fef3983d
Stephen Campbell <sac170630@utdallas.edu>
2022-02-13 17:25:42 -0600

    Removed Build from VC

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-l"ti/grlib/lib/ticlang/m4f/grlib.a" -Wl,-l"third_party/spiffs/lib/ticlang/m4f/spiffs_cc26xx.a" -Wl,-l"ti/drivers/lib/ticlang/m4f/drivers_cc13x2x7.a" -Wl,-lti_utils_build_linker.cmd.genlibs $(GEN_CMDS__FLAG) -Wl,-l"ti/devices/cc13x2x7_cc26x2x7/driverlib/bin/ticlang/driverlib.lib" -Wl,-llibc.a
-
